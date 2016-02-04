b6efa183175e10d85fb1736f9e5be3c9fa993d4a
dstefani <don@fiftyflowers.com>
2016-02-04 11:12:53 -0700

    majority of JS now a module

---
+++ js/custom_controllers.js
@@ -0,0 +1,67 @@
+var slideData = function() {
+  // onLoad
+  this.sliderOnLoad = function(data, prevButton, nextButton) {
+    // onLoad set default opacity
+    prevButton.css( "opacity", '0.0');
+    nextButton.css( "opacity", '0.0');
+    var numOfSlides = data.data.numberOfSlides;
+    var curSlide = 1;
+    var btnObj = getSliderState(data, curSlide, numOfSlides);
+    mouseEnterField(btnObj, prevButton, nextButton);
+  };
+
+  // get info from iosSlider
+  this.sliderOnChange = function(data, prevButton, nextButton) {
+    var numOfSlides = data.data.numberOfSlides;
+    var curSlide = (data.currentSlideNumber) ? data.currentSlideNumber : 1;
+    var btnObj = getSliderState(data, curSlide, numOfSlides);
+    // set the current onClick opacity
+    prevButton.css( "opacity", btnObj.prevO);
+    nextButton.css( "opacity", btnObj.nextO);
+    // give the opacity state to mouse in/out
+    mouseEnterField(btnObj, prevButton, nextButton);
+  };
+
+  /*
+  Get slide state and the opacity values
+  Need to know the opacity upon mouseleave
+  so when mouseenter again the opacity will match the state of the slide
+  */
+  function mouseEnterField(btnObj, prevButton, nextButton) {
+
+    $('.iosSlider').unbind().mouseenter(function() {
+      prevButton.fadeTo( "slow", btnObj.prevO);
+      nextButton.fadeTo( "slow", btnObj.nextO);
+    }).mouseleave(function() {
+      prevButton.fadeTo( "slow", "0.0");
+      nextButton.fadeTo( "slow", "0.0");
+    });
+  }
+
+  function getSliderState(data, curSlide, numOfSlides) {
+
+    var prevSlide = data.prevSlideNumber;
+    var prevOpacity, nextOpacity;
+
+    if (curSlide === 1) {
+      prevOpacity = '0.5';
+      nextOpacity = '1.0';
+    }
+    else if (curSlide != numOfSlides && prevSlide != undefined) {
+      prevOpacity = '1.0';
+      nextOpacity = '1.0';
+    }
+    else if (curSlide === numOfSlides) {
+      prevOpacity = '1.0';
+      nextOpacity = '0.5';
+    }
+    var buttonObj = {
+      prevO: prevOpacity,
+      nextO: nextOpacity
+    };
+
+    return buttonObj;
+  }
+
+
+};
